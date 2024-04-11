5a4ef6c35cb920b4218ec090db64f1a3483163dc
Tiki102 <tijana.laporte.m@gmail.com>
2024-04-11 22:46:55 +0200

    Ajout des attributs nécessaires de Post

--- database/migrations/2024_04_10_145041_create_posts_table.php
+++ database/migrations/2024_04_10_145041_create_posts_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('picture');
+            $table->string('content');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('no action')->OnUpdate('cascade');
             $table->timestamps();
         });
     }
