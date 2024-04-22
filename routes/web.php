d71fddfffec7c5409bed2f30b6da98f093e8c996
Tiki102 <tijana.laporte.m@gmail.com>
2024-04-22 11:44:20 +0200

    création de la route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -27,5 +30,9 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+// Créer une route pour visualiser le contenu d’un Post
+Route::get('/post/1', [PostController::class, 'show'])->name('post.show');
+
 
 require __DIR__.'/auth.php';
